67e10e41d491b32c08981ee9e3f610db9ab28c17
YOKOTA, Takehiko <skirnir@gmail.com>
2006-07-14 03:51:31 +0000

    BeanTableをBeantableに変更した。
    git-svn-id: https://www.seasar.org/svn/sandbox/cms/trunk@167 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ beantable/src/test/java/org/seasar/cms/beantable/impl/BeantableImplTest.java
@@ -0,0 +1,86 @@
+package org.seasar.cms.beantable.impl;
+
+import org.seasar.cms.beantable.Beantable;
+import org.seasar.cms.database.identity.Identity;
+import org.seasar.cms.database.identity.impl.HsqlIdentity;
+import org.seasar.extension.unit.S2TestCase;
+
+public class BeantableImplTest extends S2TestCase {
+
+    private Identity identity_;
+
+    protected Beantable newBeanTable(Class beanClass) {
+
+        BeantableImpl beanTable = new BeantableImpl(beanClass);
+        beanTable.setIdentity(identity_);
+        beanTable.setDataSource(getDataSource());
+        return beanTable;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        // TODO Auto-generated method stub
+        super.setUp();
+        include("j2ee.dicon");
+    }
+
+    @Override
+    protected void setUpAfterContainerInit() throws Throwable {
+        super.setUpAfterContainerInit();
+
+        identity_ = new HsqlIdentity();
+        identity_.setDataSource(getDataSource());
+        identity_.startUsingDatabase();
+    }
+
+    @Override
+    protected void tearDownBeforeContainerDestroy() throws Throwable {
+
+        identity_.stopUsingDatabase();
+
+        super.tearDownBeforeContainerDestroy();
+    }
+
+    public void testActivate() throws Exception {
+
+        try {
+            newBeanTable(Hoge.class).activate();
+        } catch (Throwable t) {
+            fail("正常にactivate()できること");
+        }
+    }
+
+    public void testCreateTable() throws Exception {
+
+        Beantable beanTable = newBeanTable(Hoge.class);
+        beanTable.activate();
+        beanTable.dropTable(true);
+
+        assertTrue(beanTable.createTable());
+        assertFalse("テーブルが存在する場合は何もしないこと", beanTable.createTable());
+    }
+
+    public void testCreateTable2() throws Exception {
+
+        Beantable beanTable = newBeanTable(Hoge2.class);
+        beanTable.activate();
+        beanTable.dropTable(true);
+
+        try {
+            beanTable.createTable();
+        } catch (Throwable t) {
+            t.printStackTrace();
+            fail("IDカラムがシーケンスに対応付けられている場合に正常にテーブルを生成できること");
+        }
+    }
+
+    public void testDropTable() throws Exception {
+
+        Beantable beanTable = newBeanTable(Hoge.class);
+        beanTable.activate();
+        beanTable.createTable(true);
+
+        assertTrue(beanTable.dropTable());
+        assertFalse("テーブルが存在しない場合は何もしないこと", beanTable.dropTable());
+    }
+}
