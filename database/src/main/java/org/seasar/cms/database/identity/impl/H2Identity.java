98f1098f66a1da131c791561002586b30fb1b936
YOKOTA, Takehiko <skirnir@gmail.com>
2006-10-12 13:09:20 +0000

    H2用のIdentityを追加した。
    git-svn-id: https://www.seasar.org/svn/sandbox/cms/trunk@401 ce2cb714-bd0d-0410-8c66-e472ade7ad34

---
+++ database/src/main/java/org/seasar/cms/database/identity/impl/H2Identity.java
@@ -0,0 +1,88 @@
+package org.seasar.cms.database.identity.impl;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.commons.dbutils.DbUtils;
+
+/**
+ * <p>
+ * <b>同期化：</b> このクラスはスレッドセーフです。
+ * </p>
+ *
+ * @author YOKOTA Takehiko
+ */
+public class H2Identity extends AbstractIdentity {
+
+    public String getDatabaseProductId() {
+        return "h2";
+    }
+
+    public boolean isMatched(String productName, String productVersion) {
+        if ("H2".equals(productName)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean existsTable(String tableName) throws SQLException {
+        return super.existsTable(tableName.toUpperCase());
+    }
+
+    @Override
+    public String[] getColumns(String tableName) throws SQLException {
+        return super.getColumns(tableName.toUpperCase());
+    }
+
+    @Override
+    public SQLToDefineIdColumn getSQLToDefineIdColumn(String tableName,
+        String columnName, String sequenceName) {
+        if (sequenceName == null) {
+            return new SQLToDefineIdColumn("INTEGER NOT NULL IDENTITY",
+                new String[0]);
+        } else {
+            return super.getSQLToDefineIdColumn(tableName, columnName,
+                sequenceName);
+        }
+    }
+
+    @Override
+    public SQLToDeleteIdColumn getSQLToDeleteIdColumn(String tableName,
+        String columnName, String sequenceName) {
+        if (sequenceName == null) {
+            return new SQLToDeleteIdColumn(new String[0]);
+        } else {
+            return super.getSQLToDeleteIdColumn(tableName, columnName,
+                sequenceName);
+        }
+    }
+
+    @Override
+    public void stopUsingDatabase() {
+        Connection con = null;
+        Statement st = null;
+        try {
+            con = ds_.getConnection();
+            st = con.createStatement();
+            st.executeUpdate("SHUTDOWN COMPACT");
+        } catch (SQLException ex) {
+            throw new RuntimeException("Can't shutdown", ex);
+        } finally {
+            DbUtils.closeQuietly(con, st, null);
+        }
+    }
+
+    @Override
+    public String getSQLToGetGeneratedId(String tableName, String columnName,
+        String sequenceName) {
+        if (sequenceName == null) {
+            return "CALL IDENTITY()";
+        } else {
+            return super.getSQLToGetGeneratedId(tableName, columnName,
+                sequenceName);
+        }
+    }
+}
