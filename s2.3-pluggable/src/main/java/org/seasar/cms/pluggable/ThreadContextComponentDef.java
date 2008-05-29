5252ee81db66a4849f47fdfbbc2e96ca1989bf79
YOKOTA, Takehiko <skirnir@gmail.com>
2008-05-29 03:59:47 +0000

    ThreadContextComponentDefのコンストラクタでsuperを呼んでいる部分で、不適切なスーパーコンストラクタを呼んでいた問題を修正した。
    git-svn-id: https://www.seasar.org/svn/sandbox/cms/trunk@820 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- s2.3-pluggable/src/main/java/org/seasar/cms/pluggable/ThreadContextComponentDef.java
+++ s2.3-pluggable/src/main/java/org/seasar/cms/pluggable/ThreadContextComponentDef.java
@@ -7,13 +7,13 @@ public class ThreadContextComponentDef extends SimpleComponentDef {
     private Class componentClass_;
 
     public ThreadContextComponentDef(Class componentClass, String componentName) {
-        super(componentClass, componentName);
+        super(null, componentClass, componentName);
         componentClass_ = componentClass;
     }
 
     public Object getComponent() {
         return ((ThreadContext) getContainer().getRoot().getComponent(
-            ThreadContext.class)).getComponent(componentClass_);
+                ThreadContext.class)).getComponent(componentClass_);
     }
 
     public void destroy() {
