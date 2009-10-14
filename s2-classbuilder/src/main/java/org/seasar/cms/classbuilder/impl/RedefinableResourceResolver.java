8224794cd8b5eed92091c3e79ecf52205e261c45
YOKOTA, Takehiko <skirnir@gmail.com>
2009-10-14 05:13:05 +0000

    [#CMS-41]に対処した。
    git-svn-id: https://www.seasar.org/svn/sandbox/cms/trunk@1029 ce2cb714-bd0d-0410-8c66-e472ade7ad34

--- s2-classbuilder/src/main/java/org/seasar/cms/classbuilder/impl/RedefinableResourceResolver.java
+++ s2-classbuilder/src/main/java/org/seasar/cms/classbuilder/impl/RedefinableResourceResolver.java
@@ -1,18 +1,22 @@
 package org.seasar.cms.classbuilder.impl;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.seasar.cms.classbuilder.util.S2ContainerBuilderUtils;
 import org.seasar.framework.container.factory.ClassPathResourceResolver;
+import org.seasar.framework.env.Env;
 import org.seasar.framework.exception.IORuntimeException;
+import org.seasar.framework.util.ResourceUtil;
 
+public class RedefinableResourceResolver extends ClassPathResourceResolver {
+    private static final char COLON = ':';
 
-public class RedefinableResourceResolver extends ClassPathResourceResolver
-{
-    public InputStream getInputStream(String path)
-    {
+    public InputStream getInputStream(String path) {
         String[] paths = constructRedefinedDiconPaths(path);
         for (int i = 0; i < paths.length; i++) {
             try {
@@ -29,11 +33,55 @@ public class RedefinableResourceResolver extends ClassPathResourceResolver
         return super.getInputStream(path);
     }
 
+    /**
+     * クラスパスから読み込み対象となるリソースを取得し、URLを構築します。 取得する際には、拡張子の手前に環境名をサフィックスを加えたパス(例
+     * env.txt→env_ut.txt)を用います。 環境名を加えたパスのリソースが存在しない場合は、パスをそのまま用います。
+     * 
+     * @param path
+     *            読み込み対象となるリソースのパス
+     * @return 取得したリソースのURL
+     * @see Env#adjustPath(String)
+     */
+    // 少なくともSeasar2.4.39の時点でパスに「:」を含む場合に挙動が仕様を満たさないため、ここで独自に対処を行なうようにしている。
+    @Override
+    protected URL getURL(String path) {
+        String extPath = Env.adjustPath(path);
+        URL url = toURL_corrected(extPath);
+        if (url == null && !extPath.equals(path)) {
+            url = toURL_corrected(path);
+        }
+        return url;
+    }
 
-    protected String[] constructRedefinedDiconPaths(String path)
-    {
+    URL toURL_corrected(final String path) {
+        if (path.indexOf(COLON) >= 0) {
+            try {
+                URL url = new URL(path);
+                InputStream is = null;
+                try {
+                    is = url.openStream();
+                    return url;
+                } catch (IOException ex) {
+                    // 開けなかった場合はそのリソースを使わないようにする。
+                    return null;
+                } finally {
+                    if (is != null) {
+                        try {
+                            is.close();
+                        } catch (IOException ignore) {
+                        }
+                    }
+                }
+            } catch (MalformedURLException ignore) {
+                // URLの形式でない場合はクラスパス上のリソースとみなす。
+            }
+        }
+        return ResourceUtil.getResourceNoException(path);
+    }
+
+    protected String[] constructRedefinedDiconPaths(String path) {
         int delimiter = path
-            .lastIndexOf(RedefinableXmlS2ContainerBuilder.DELIMITER);
+                .lastIndexOf(RedefinableXmlS2ContainerBuilder.DELIMITER);
         int slash = path.lastIndexOf('/');
         if (delimiter >= 0 && delimiter > slash) {
             // リソース名に「+」が含まれていない場合だけ特別な処理を行なう。
@@ -52,14 +100,14 @@ public class RedefinableResourceResolver extends ClassPathResourceResolver
             suffix = path.substring(dot);
         }
         String resourceBody = S2ContainerBuilderUtils
-            .fromURLToResourcePath(body
-                + RedefinableXmlS2ContainerBuilder.DELIMITER + suffix);
+                .fromURLToResourcePath(body
+                        + RedefinableXmlS2ContainerBuilder.DELIMITER + suffix);
         if (resourceBody != null) {
             // パスがJarのURLの場合はURLをリソースパスに変換した上で作成したパスを候補に含める。
             pathList.add(resourceBody);
         }
         pathList
-            .add(body + RedefinableXmlS2ContainerBuilder.DELIMITER + suffix);
+                .add(body + RedefinableXmlS2ContainerBuilder.DELIMITER + suffix);
         return pathList.toArray(new String[0]);
     }
 }
